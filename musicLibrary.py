2a5a4eb058f315235d2c8bc48ce2ac0b7769558b
unknown <alaminhossain2000and@gmail.com>
2024-07-04 23:38:17 +0600

    first commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "in":"https://www.youtube.com/watch?v=eVTXPUF4Oz4",
+    "let":"https://www.youtube.com/watch?v=RBumgq5yVrA",
+    "calm":"https://www.youtube.com/watch?v=WcIcVapfqXw",
+    "mockingbird":"https://www.youtube.com/watch?v=S9bCLPwzSC0"
+}
